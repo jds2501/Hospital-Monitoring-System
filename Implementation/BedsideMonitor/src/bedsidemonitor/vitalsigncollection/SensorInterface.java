36b548a38d25a7381872ae6c4b632563bdf30fe7
Jason Smith <jds2501@rit.edu>
2011-11-01 19:01:28 -0400

    Added sensor poll logic

---
+++ Implementation/BedsideMonitor/src/bedsidemonitor/vitalsigncollection/SensorInterface.java
@@ -0,0 +1,30 @@
+/*
+ * Package: bedsidemonitor.vitalsigncollection
+ *
+ * File: SensorInterface.java
+ *
+ * Date: Nov 1, 2011
+ * 
+ */
+package bedsidemonitor.vitalsigncollection;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+/**
+ * Abstract interface to allow callers to get a vital sign measurement
+ * off of a sensor.
+ * 
+ * @author Jason Smith
+ */
+public interface SensorInterface extends Remote {
+
+    /**
+     * Gets the vital sign off of the sensor.
+     * 
+     * @return an integer value of 0 - 100 off of the sensor
+     */
+    public int getVitalSign() throws RemoteException;
+
+} // SensorInterface
---
+++ Implementation/BedsideMonitor/src/bedsidemonitor/vitalsigncollection/VitalSignCollectionController.java
@@ -0,0 +1,61 @@
+/*
+ * Package: bedsidemonitor.vitalsigncollection
+ *
+ * File: VitalSignCollectionController.java
+ *
+ * Date: Nov 1, 2011
+ * 
+ */
+package bedsidemonitor.vitalsigncollection;
+
+import java.rmi.RemoteException;
+import java.util.Queue;
+
+
+/**
+ * Collection controller in charge of gathering data against a
+ * sensor interface and pushing the results to a vital sign
+ * message queue.
+ * 
+ * @author Jason Smith
+ */
+public class VitalSignCollectionController {
+
+    /**
+     * Sensor interface to poll data from
+     */
+    private SensorInterface sensor;
+    
+    /**
+     * Vital sign message queue to push sensor data to
+     */
+    private Queue<Integer> vitalSignMsgQueue;
+    
+    /**
+     * Constructs a VitalSignCollectionController object with a
+     * sensor interface and a vital sign message queue.
+     * 
+     * @param sensor the sensor interface to pull sensor data from
+     * @param vitalSignMsgQueue the message queue to push sensor data to
+     */
+    public VitalSignCollectionController(SensorInterface sensor, 
+            Queue<Integer> vitalSignMsgQueue){
+        this.sensor = sensor;
+        this.vitalSignMsgQueue = vitalSignMsgQueue;
+    }
+    
+    /**
+     * Polls the sensor data off the sensor interface and pushes
+     * the results to the vital sign message queue.
+     */
+    public void pollSensorData(){
+        try {
+            int reading = this.sensor.getVitalSign();
+            this.vitalSignMsgQueue.offer(reading);
+        } catch (RemoteException ex) {
+            //TODO: Need to properly handle a failure to connect
+            ex.printStackTrace();
+        }
+    }
+    
+} // VitalSignCollectionController
