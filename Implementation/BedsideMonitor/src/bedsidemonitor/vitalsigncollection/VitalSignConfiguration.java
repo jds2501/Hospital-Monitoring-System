5ef93bd13ae5e3d174ca41b53d08f022b4d05f9a
Jason Smith <jds2501@rit.edu>
2011-11-03 13:50:52 -0400

    Partial completion of vital sign processing logic.

--- Implementation/BedsideMonitor/src/bedsidemonitor/vitalsigncollection/VitalSignConfiguration.java
+++ Implementation/BedsideMonitor/src/bedsidemonitor/vitalsigncollection/VitalSignConfiguration.java
@@ -1,25 +1,22 @@
-package bedsidemonitor.scaling;
-
-import nursestation.enums.Vitals;
+package bedsidemonitor.vitalsigncollection;
 
 /**
  *
  * Interface for the algorithms responsible for converting raw
  * SensorData to a usable form
  *
- * @author ian hunt
+ * @author Ian hunt
  * @date 11/3/11
  */
-public interface VitalSignConverter {
+public interface VitalSignConfiguration {
 
     /**
      * Convert a raw sensor value
+     * 
      * @param value raw data in integer form
-     * @param vitalType vital sign type to convert
+     * 
      * @return converted value
      */
-    public double convert( int value, Vitals vitalType );
-
-
+    public double convert( int value );
 
 }
---
+++ Implementation/BedsideMonitor/src/bedsidemonitor/vitalsigncollection/VitalSignProcessing.java
@@ -0,0 +1,43 @@
+/*
+ * Package: bedsidemonitor.vitalsigncollection
+ *
+ * File: VitalSignProcessing.java
+ *
+ * Date: Nov 3, 2011
+ * 
+ */
+package bedsidemonitor.vitalsigncollection;
+
+import java.util.Observable;
+import java.util.Queue;
+
+
+/**
+ * @author Jason
+ *
+ */
+public class VitalSignProcessing extends Observable {
+
+    /**
+     * Vital sign message queue to containing gathered sensor data
+     */
+    private Queue<Integer> vitalSignMsgQueue;
+    
+    private Double vitalSignValue;
+    
+    private VitalSignConfiguration converter;
+    
+    public VitalSignProcessing(Queue<Integer> vitalSignMsgQueue,
+            VitalSignConfiguration converter){
+        this.vitalSignMsgQueue = vitalSignMsgQueue;
+        this.vitalSignValue = null;
+        this.converter = converter;
+    }
+    
+    public void pullVitalSign(){
+        int rawVitalSignReading = vitalSignMsgQueue.poll();
+        vitalSignValue = converter.convert(rawVitalSignReading);
+        // TODO: Finish the processing post conversion
+    }
+    
+}
