cd1decacee5778f30dfdf1fe72e4e25b83be5623
Chris Bentivenga <chris.bentivenga@gmail.com>
2011-11-03 11:58:44 -0400

    Re-added the stub code that got accidentally removed

--- Implementation/BedsideMonitor/src/bedsidemonitor/sensor/SensorImpl_Stub.java
+++ Implementation/BedsideMonitor/src/bedsidemonitor/sensor/SensorImpl_Stub.java
@@ -1,5 +1,6 @@
 package bedsidemonitor.sensor;
 
+import java.lang.reflect.Method;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -7,10 +8,38 @@ public class SensorImpl_Stub
 extends java.rmi.server.RemoteStub
 implements SensorInterface, Remote{
 
+	private static final long serialVersionUID = 1L;
+	private static Method $method_getVitalSign_0;
+
+	static {
+		try {
+			$method_getVitalSign_0 = SensorInterface.class.getMethod("getVitalSign",
+					new java.lang.Class[] {});
+		} catch (java.lang.NoSuchMethodException e) {
+			throw new java.lang.NoSuchMethodError(
+					"stub class initialization failed");
+		}
+	}
+
+	
+    public SensorImpl_Stub(java.rmi.server.RemoteRef ref) {
+    	super(ref);
+    }
+    
+    
 	@Override
 	public int getVitalSign() throws RemoteException {
-		// TODO Auto-generated method stub
-		return 0;
+		  Object $result;
+		try {
+			$result = ref.invoke(this, $method_getVitalSign_0, null, $method_getVitalSign_0.hashCode());
+			  return (Integer.parseInt($result.toString()));
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return -100000;
 	}
 
 }
